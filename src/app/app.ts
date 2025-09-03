fa5bfeabc98385c7ba706ee34b9779892cb08c3a
Ad-Gpta <aditibhoomika@gmail.com>
2025-09-03 10:55:23 +0000

    added Angular material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet, Home, MatSlideToggleModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
