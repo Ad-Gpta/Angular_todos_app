90b7dbd4c5c3864f9454a4e2c8d664135be6b0d3
Ad-Gpta <aditibhoomika@gmail.com>
2025-09-03 11:15:40 +0000

    add basic toolbar from angular material

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,17 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+/**
+ * @title Toolbar overview
+ */
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, MatSlideToggleModule],
+  imports: [RouterOutlet, Home, MatSlideToggleModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
