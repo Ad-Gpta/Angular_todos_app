19169ad02a70e96e9dbba4965d2ee29a73d3f278
Ad-Gpta <aditibhoomika@gmail.com>
2025-09-04 08:16:42 +0000

    add addTodo form

--- src/app/todo-add-edit/todo-add-edit.ts
+++ src/app/todo-add-edit/todo-add-edit.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-todo-add-edit',
-  imports: [MatIconModule, MatDialogModule],
+  imports: [MatIconModule, MatDialogModule, MatButtonModule, MatFormFieldModule, MatSelectModule,
+    MatInputModule, MatDatepickerModule],
   templateUrl: './todo-add-edit.html',
-  styleUrl: './todo-add-edit.css'
+  styleUrl: './todo-add-edit.css',
+  providers: [provideNativeDateAdapter()],
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodoAddEdit {
-
 }
